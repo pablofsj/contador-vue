5c34d55aa738d0474ee4f96660bcb241369e332c
pablofsj <52142556+pablofsj@users.noreply.github.com>
2020-07-15 13:47:52 -0400

    Subida de ejercicio

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Contador/>
+  </div>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<style>
+#app {
+  
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1>Juego del Contador</h1>
+        <br>
+        <h5>Al clickear el boton aumentan los clicks totales y al hacer mouseover sobre el boton, el contador se reinicia a 0</h5>
+        <br>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <button  @click="sumarClicks()" @mouseenter="reiniciarClicks()" type="button" class="btn btn-outline-info">¡Clickeame!</button>
+        <br><br>
+      </div>
+      <div class="col-sm-12">
+        <h2>Clicks Totales: <span>{{clicksTotales}}</span></h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name:'Contador',
+  data:function() {
+    return {
+      clicksTotales:0
+    }
+  },
+  methods: {
+    sumarClicks: function(){
+      this.clicksTotales++;
+
+    },
+
+    reiniciarClicks: function(){
+      this.clicksTotales=0;
+
+    }
+
+  }
+
+};
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h1{
+  color: rgb(34, 191, 212);
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
